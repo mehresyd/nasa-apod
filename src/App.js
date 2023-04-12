8859859d05299de170db8d2d234f5475ea70004f
mehresyd <93683641+mehresyd@users.noreply.github.com>
2023-04-12 18:01:26 -0500

    Calling to NASA API with useEffect hook on component load

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { getPictureOfTheDay } from './api';
 
 function App() {
+  React.useEffect(() => {
+    console.log('use effect')
+
+    const getPicture = async () => {
+      const response = await getPictureOfTheDay()
+      console.log('response in use effect', response)
+    }
+
+    getPicture()
+  }, [])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>NASA Astornomy Picture of the Day</h1>
+     <img src={''}/>
     </div>
   );
 }
--- src/api.js
+++ src/api.js
@@ -3,7 +3,11 @@ import {NASA_API_KEY} from "./secrets";
 const API_URL_BASE = "https://api.nasa.gov/planetary/apod";
 
 export const getPictureOfTheDay = async () => {
+    //this is an example of string templating 
+    //combining JS variables and plain text to make a new string
     const apiUrl = `${API_URL_BASE}?api_key=${NASA_API_KEY}`;
+
+    //async await = updates/nicer promises
     const response = await fetch(apiUrl);
 
     console.log('response from NASA: ', response);
